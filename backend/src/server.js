70a7ffb2ce6e5e17edef94c76fe973d633efde30
TausifAli2000 <tausifali2000@gmail.com>
2025-05-05 14:57:01 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,6 +25,14 @@ app.use(express.json());
 
 app.use("/api/v1", userRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
