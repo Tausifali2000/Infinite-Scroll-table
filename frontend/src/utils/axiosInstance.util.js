1bb863c8afacb37bb1beb43030830be9e757874f
TausifAli2000 <tausifali2000@gmail.com>
2025-05-05 15:27:14 +0530

    changes

--- frontend/src/utils/axiosInstance.util.js
+++ frontend/src/utils/axiosInstance.util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api/v1";
 
 
 export const axiosInstance = axios.create({
